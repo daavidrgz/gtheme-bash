1a78d7a0e4a9cff6514bf24e75a2a36adccc1775
David <david.rbacelar@udc.es>
2021-10-12 01:06:53 +0200

    Update: Wallpaper script

--- post-scripts/wallpaper.sh
+++ post-scripts/wallpaper.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 Y="\e[1;33m"
 W="\e[0m"
 W_B="\e[1m"
 
-WALLPAPER_URL=~/.config/gtheme/wallpapers/Gruvbox/light-archlinux.png
-feh --no-fehbg --bg-fill "$WALLPAPER_URL"
-
-# Flag to not execute the dm wallpaper update after every boot
-[ "$1" == "no-dm" ] && exit 0
+WALLPAPER_URL=$1
+feh --bg-fill "$WALLPAPER_URL"
 
 echo -e "${Y}•${W} Root privileges are needed in order to apply the wallpaper to lightdm webkit theme aether!\n"
 LIGHTDM_WALLPAPER=/usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether/src/img/wallpapers/wallpaper
