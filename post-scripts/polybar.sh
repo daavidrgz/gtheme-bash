2528647dff5d5651db98e8deeaf1da72a3a1badb
David <david.rbacelar@udc.es>
2021-10-15 18:05:42 +0200

    New feature: Change all desktop dotfiles

--- post-scripts/polybar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while pgrep -u $UID -x polybar &>/dev/null; do
-	killall -q polybar
-	sleep 1
-done
-nohup polybar -q main -c $HOME/.config/polybar/config.ini &
-
-exit 0
--- post-scripts/spotify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-spicetify update
-exit 0
